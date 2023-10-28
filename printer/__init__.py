325d9aac94154a748428b6918e0ffb72408fe7e8
EOF-D <END-OFD@pm.me>
2023-10-28 17:40:55 -0400

    chore: initial commit

---
+++ printer/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from .nodes import *
+from .parser import *
+
+__version__ = "v0.1.0"
+__author__ = "EOF-D <END-OFD@pm.me>"
+__license__ = "MIT"
---
+++ printer/__main__.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import argparse, os
+import rich
+
+from lark import Lark
+from printer import __version__, __author__, __license__
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(prog="printer")
+    parser.add_argument("file", type=argparse.FileType("r"))
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"Printer: {__version__} by {__author__} under {__license__}",
+    )
+
+    args = parser.parse_args()
+
+    if args.file:
+        path = os.path.dirname(__file__)
+
+        with open(f"{path}/parser/grammar.lark", "r") as fp:
+            parser = Lark(fp.read(), start="template", parser="lalr")
+        
+        rich.inspect(parser.parse(args.file.read()))
+
+
+if __name__ == "__main__":
+    main()
